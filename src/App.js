a8e9cdb55452abd0e1e14d5054e279eade276a2f
Raul Veras <verasraul@gmail.com>
2022-03-29 17:43:07 -0400

    fixed search engine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import RandomStock from './components/RandomStock';
+import SearchResults from './components/SearchResults';
+import { Search } from './services/Constants';
+import SearchStocks from './components/SearchStocks';
+import StockList from './components/StockList';
+
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className='links'>
+        {/* 'NavLink' links your pages. */}
+        <nav>
+          <NavLink to={"/"} >Home</NavLink>
+          <NavLink to={"/search"} >Search-Stocks</NavLink>
+          <NavLink to={"/random"} >Random-Stock</NavLink>
+        </nav>  
+      </div>
+
+      <div>
+      {/* // 'Routes' renders your pages. */}
+      <Routes>
+      // 'Route' in singular form is a self-closing tag.
+        <Route path='/' element={<Home /> } />
+        <Route path='search' element={<SearchResults /> } />
+        <Route path='random' element={<RandomStock /> } />
+      </Routes>
+     
+      </div>
+
+
     </div>
   );
 }
